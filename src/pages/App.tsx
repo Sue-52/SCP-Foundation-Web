d3407a6fa448db5801e6e215bce4a09d2bcf608a
Sue-52 <1219243947@qq.com>
2022-05-12 08:42:48 +0800

    submit：修改部分设置-eslint、vite、tsconfig

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,11 +2,11 @@
 
 // Layout 页面
 function App() {
-    return (
-        <div>
-            <h1>App</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  )
 }
 
 export default App
